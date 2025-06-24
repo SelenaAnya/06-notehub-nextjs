6521f290367eec889f655602addb5a78ed318fe2
Anna Kovalenko <91620969+SelenaAnya@users.noreply.github.com>
2025-06-24 20:49:55 +0300

    2406 2049

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,21 +1,10 @@
-// import {
-//     HydrationBoundary,
-//     QueryClient,
-//     dehydrate,
-//   } from '@tanstack/react-query';
 import { fetchNotes } from '@/lib/api';
 import NotesClient from './Notes.client';
 
 export default async function NotesPage() {
   const initialData = await fetchNotes(1, '');
-  const notes = await getNotes()
-  return (
-    <div>
-      <h1>Notes</h1>
-      <br />
-      <NotesClient items={notes} />
-    </div>
-  )
-}
 
-export default NotesPage;
+  return (
+    <NotesClient initialData={initialData} />
+  );
+}
